9b53d23acc581fc11215621dabfedeeeada59b94
Sheshant-Manure <msheshant1997@gmail.com>
2023-05-17 15:57:14 +0530

    updated mongoose from local to atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/todoList');
- // await mongoose.connect('mongodb+srv://msheshant1997:Sheshant123@cluster0.vhl81py.mongodb.net/?retryWrites=true&w=majority');
+ //   await mongoose.connect('mongodb://127.0.0.1:27017/todoList');
+ await mongoose.connect('mongodb+srv://msheshant1997:Sheshant123@cluster0.vhl81py.mongodb.net/?retryWrites=true&w=majority');
 }
